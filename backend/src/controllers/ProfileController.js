d1e02684ad2a922ce29492c943dddf49ae843a4c
matheus <matheus.pitta12@gmail.com>
2019-10-21 15:04:38 -0300

    Fim node

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -6,5 +6,13 @@ module.exports = {
 
     const spots = await Spot.find({ user : user_id });
     return res.json(spots);
+
+    /* const booking = await Booking.create({
+      user: user_id,
+      spot: spot_id,
+      date,
+    })
+    */
+    // await booking.populate('spot').populate('user').execPopulate();
   }
 }
